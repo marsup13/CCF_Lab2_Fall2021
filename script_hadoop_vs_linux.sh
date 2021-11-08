774c6da07554e38acb7b1a64c76ff0de1bd61385
marsup13 <marwane.adala@supcom.tn>
2021-11-07 21:52:44 -0500

    updating the script for hadoop vs linux

--- script_hadoop_vs_linux.sh
+++ script_hadoop_vs_linux.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
-time cat $1 | tr ' ' '\n' | sort | uniq -c 
-time hadoop jar hadoop-mapreduce-examples-3.3.1.jar wordcount /input/4vxdw3pa out4vxdw3pa
-hdfs dfs -rm -r -skipTrash -f outweh83uyn
+
+FILES="Datasets/4vxdw3pa
+Datasets/kh9excea
+Datasets/dybs9bnk
+Datasets/datumz6m
+Datasets/j4j4xdw6
+Datasets/ym8s5fm4
+Datasets/2h6a75nk
+Datasets/vwvram8
+Datasets/weh83uyn"
+for f in $FILES
+do
+	time hadoop jar hadoop-mapreduce-examples-3.3.1.jar wordcount /input/$f outfile
+	hdfs dfs -rm -r -skipTrash -f outfile
+done
+
+for f in $FILES
+do
+	time cat $f | tr ' ' '\n' | sort | uniq -c
+	
+done
+ 
+
+
